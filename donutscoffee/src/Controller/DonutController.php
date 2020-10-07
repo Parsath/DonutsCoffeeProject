aa5a6bf55cf3cb6f40b86a3b1f554022edd3ea76
Parsath <bilel.taktak@esprit.tn>
2020-10-07 21:21:02 +0100

    integrated my front-end work in the symfony project

--- donutscoffee/src/Controller/DonutController.php
+++ donutscoffee/src/Controller/DonutController.php
@@ -4,17 +4,26 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DonutController
+class DonutController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('Works!');
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/order", name="app_order")
+     */
+    public function orderpage()
+    {
+        return $this->render('home/order.html.twig');
     }
 
 }
---
+++ donutscoffee/src/Controller/DonutOrderController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DonutOrderController extends AbstractController
+{
+//    /**
+//     * @Route("/order", name="app_order")
+//     */
+//    public function homepage()
+//    {
+//        return $this->render('home/order.html.twig');
+//    }
+}
